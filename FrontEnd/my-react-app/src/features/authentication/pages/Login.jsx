3a4475311280c91a9345e3108782b811000ef2d6
maazhabibkhan <maazhabiboffical@gmail.com>
2026-09-10 12:57:20 +0500

    Add frontend login page

---
+++ FrontEnd/my-react-app/src/features/authentication/pages/Login.jsx
@@ -0,0 +1,20 @@
+import LoginForm from "../components/LoginForm";
+
+const Login = () => {
+
+    return (
+        <div>
+            <h1>
+                SmallPOS
+            </h1>
+
+            <h2>
+                Login
+            </h2>
+
+            <LoginForm />
+        </div>
+    );
+};
+
+export default Login;
